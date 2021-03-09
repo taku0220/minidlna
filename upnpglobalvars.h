1ef9ca52cbbe778770f09cb091b53a1a1fc96abd
taku0220 <taku0220.sub@gmail.com>
2021-03-09 11:24:53 +0900

    patch apply SourceForge #92 "https://sourceforge.net/p/minidlna/patches/_discuss/thread/28a9f79d/038b/attachment/video_thumbnail-1.1.4.patch"

--- upnpglobalvars.h
+++ upnpglobalvars.h
@@ -194,6 +194,10 @@ extern uint32_t runtime_flags;
 #define NO_PLAYLIST_MASK      0x0008
 #define SYSTEMD_MASK          0x0010
 #define MERGE_MEDIA_DIRS_MASK 0x0020
+#ifdef THUMBNAIL_CREATION
+#define THUMB_MASK            0x0040
+#define THUMB_FILMSTRIP       0x0080
+#endif
 #define WIDE_LINKS_MASK       0x0040
 #ifdef HAVE_AVAHI
 #define TIVO_BONJOUR_MASK     0x0080
